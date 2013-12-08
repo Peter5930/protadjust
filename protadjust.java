54496f18eea0dd8258ecad05953dc86f65df51f8
Peter5930 <peter.mackenzie@gmail.com>
2013-12-08 15:59:25 +0000

    Update protadjust.java
    -Added a nerf for poison so that it isn't OP without the protective effects of prot.
    
    -Added a buff for thorns.

--- protadjust.java
+++ protadjust.java
@@ -1,6 +1,8 @@
 package io.github.protadjust;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,71 +16,81 @@ import org.bukkit.event.entity.EntityDamageEvent;
 
 public final class Protadjust extends JavaPlugin implements Listener {
 
-	 public void onEnable()
-	 {
-	 this.getServer().getPluginManager().registerEvents(this, this);
-	 }
-	
-	 @EventHandler(priority = EventPriority.LOWEST)
-	  public void onEntityDamageEvent(EntityDamageEvent event) {
-		
-	    double damage = event.getDamage();
+         public void onEnable()
+         {
+         this.getServer().getPluginManager().registerEvents(this, this);
+         }
+        
+         @EventHandler(priority = EventPriority.LOWEST)
+         public void onEntityDamageEvent(EntityDamageEvent event) {
+                
+         double damage = event.getDamage();
 
-	    if (damage <= 0.0000001D) {
-	      return;
-	    }
-	    Entity entity = event.getEntity();
-	    if (!(entity instanceof Player)) {
-	      return;
-	    }
+         if (damage <= 0.0000001D) {
+         return;
+         }
+         Entity entity = event.getEntity();
+         if (!(entity instanceof Player)) {
+         return;
+         }
 
-	    DamageCause cause = event.getCause();
+         DamageCause cause = event.getCause();
 
-	    if (cause.equals(DamageCause.ENTITY_ATTACK)
-	    		||cause.equals(DamageCause.DROWNING)
-	    		||cause.equals(DamageCause.STARVATION)
-	    		||cause.equals(DamageCause.SUFFOCATION)){
-	    	//ENTITY_ATTACK is any kind of melee damage
-	    	//DROWNING, STARVATION and SUFFOCATION
-	    	//are unaffected by prot, so ignore these.
-	        return;
-	    }
-	    
-	    Player defender = (Player)entity;
-	    
-	    int damageticks = defender.getNoDamageTicks();   
-	    
-	    if (!(damageticks == 0))
-	    {
-	    	//Player is currently invulnerable due to recent damage
-	    	return;
-	    }
-	    
-	    PlayerInventory inventory = defender.getInventory();
-	    int enchant_level = 0;
-	    for (ItemStack armor : inventory.getArmorContents()) {
-	      enchant_level += armor.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
-	    }
-	    double damage_adjustment = 0;
-	    //damage_adjustment compensates for the prot enchant to
-	    //make the armour equivalent to vanilla diamond for
-	    //damage types other than ENTITY_ATTACK
-	    if (enchant_level >= 3 && enchant_level <= 6) {
-	    	damage_adjustment = event.getDamage() * 0.03; //adjust as necessary
-	    } else if (enchant_level >= 7 && enchant_level <= 10) {
-	    	damage_adjustment = event.getDamage() * 0.05;
-	    } else if (enchant_level >= 11 && enchant_level <= 14) {
-	    	damage_adjustment = event.getDamage() * 0.09;
-	    } else if (enchant_level >= 15) {
-	    	damage_adjustment = event.getDamage() * 0.115;
-	    }
-	    
-	    if(defender.isBlocking()){
-	    	damage_adjustment = damage_adjustment * 0.5;
-	    }
-	    
-	    double newhealth = Math.max(0, (defender.getHealth() - damage_adjustment));
+         if (cause.equals(DamageCause.ENTITY_ATTACK)
+                         ||cause.equals(DamageCause.STARVATION)
+                         ||cause.equals(DamageCause.SUFFOCATION)){
+                 //ENTITY_ATTACK is any kind of melee damage
+                 //STARVATION and SUFFOCATION
+                 //are unaffected by prot, so ignore these.
+         return;
+         }
+         
+         if (cause.equals(DamageCause.POISON)){
+        	 damage = damage * 0.2;
+         }
+        
+         Player defender = (Player)entity;
+        
+         int damageticks = defender.getNoDamageTicks();
+        
+         if (!(damageticks == 0))
+         {
+                 //Player is currently invulnerable due to recent damage
+                 return;
+         }
+        
+         PlayerInventory inventory = defender.getInventory();
+         int enchant_level = 0;
+         for (ItemStack armor : inventory.getArmorContents()) {
+         enchant_level += armor.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
+         }
+         double damage_adjustment = 0;
+         //damage_adjustment compensates for the prot enchant to
+         //make the armour equivalent to vanilla diamond for
+         //damage types other than ENTITY_ATTACK
+         if (enchant_level >= 3 && enchant_level <= 6) {
+                 damage_adjustment = damage * 0.03; //adjust as necessary
+         } else if (enchant_level >= 7 && enchant_level <= 10) {
+                 damage_adjustment = damage * 0.05;
+         } else if (enchant_level >= 11 && enchant_level <= 14) {
+                 damage_adjustment = damage * 0.09;
+         } else if (enchant_level >= 15) {
+                 damage_adjustment = damage * 0.115;
+         }
+        
+         if(defender.isBlocking()){
+                 damage_adjustment = damage_adjustment * 0.5;
+         }
+        
+         double newhealth = Math.max(0, (defender.getHealth() - damage_adjustment));
 
-	    defender.setHealth(newhealth);
-	    }
+         defender.setHealth(newhealth);
+         
+         if (cause.equals(DamageCause.THORNS)){
+             PotionEffect potionEffect = new PotionEffect(PotionEffectType.WITHER, 1, 1);
+             potionEffect.apply(defender);
+         }
+         
+         }
+         
 }

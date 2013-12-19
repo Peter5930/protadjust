3f5d2e100c2e30ad28febdaa5eb58562eef5be2a
Peter5930 <peter.mackenzie@gmail.com>
2013-12-19 03:28:45 +0000

    Update protadjust.java

--- protadjust.java
+++ protadjust.java
@@ -1,24 +1,25 @@
 package io.github.protadjust;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
-import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Arrow;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+
 
 public final class Protadjust extends JavaPlugin implements Listener {
 	
@@ -62,16 +63,15 @@ public final class Protadjust extends JavaPlugin implements Listener {
          else if (cause.equals(DamageCause.POISON)){
         	 /*Poison needs to be nerfed globally
         	  * since prot no longer protects against it
-        	  * , otherwise it would be devastating
-        	  * default  0.45*/
-            event.setDamage(event.getDamage() * config_.getDouble(poison_global_modifier));
+        	  * , otherwise it would be devastating*/
+            event.setDamage(event.getDamage() * config_.getDouble("poison_damage_global_modifier"));
          }
          
          else if (cause.equals(DamageCause.BLOCK_EXPLOSION)){
         	 /*Explosions are underpowered, even against vanilla diamond,
         	  * so they could do with being buffed.  Also, TNT is
         	  * expensive, so it's usefulness should reflect that.*/
-        	 damage = damage * 2.0;
+        	 damage = damage * config_.getDouble("explosion_damage_global_modifier");
          }
          
 		 
@@ -102,13 +102,13 @@ public final class Protadjust extends JavaPlugin implements Listener {
          //make the armour equivalent to vanilla diamond for
          //damage types other than ENTITY_ATTACK
          if (enchant_level >= 3 && enchant_level <= 6) {
-                 damage_adjustment = damage * 0.03; //adjust as necessary
+                 damage_adjustment = damage * config_.getDouble("prot_1_modifier"); //adjust as necessary
          } else if (enchant_level >= 7 && enchant_level <= 10) {
-                 damage_adjustment = damage * 0.05;
+                 damage_adjustment = damage * config_.getDouble("prot_2_modifier");
          } else if (enchant_level >= 11 && enchant_level <= 14) {
-                 damage_adjustment = damage * 0.09;
+                 damage_adjustment = damage * config_.getDouble("prot_3_modifier");
          } else if (enchant_level >= 15) {
-                 damage_adjustment = damage * 0.115;
+                 damage_adjustment = damage * config_.getDouble("prot_4_modifier");
          }
         
          if(defender.isBlocking()){
@@ -117,36 +117,37 @@ public final class Protadjust extends JavaPlugin implements Listener {
          
          if (event.getCause().equals(DamageCause.POISON) 
         		|| (event.getCause().equals(DamageCause.WITHER))){
-        	 damage_adjustment *= 3;
+        	 damage_adjustment *= config_.getDouble("poison_prot_modifier");
         	 /*Again, poison and wither are weird; this solves that*/
          }
          else if (cause.equals(DamageCause.THORNS)){
-             PotionEffect potionEffect = new PotionEffect(PotionEffectType.WITHER, 150, 0);
-             potionEffect.apply(defender);
+        	 PotionEffect potionEffect = new PotionEffect(PotionEffectType.WITHER, 150, 0);
+        	 //PotionEffect potionEffect = new PotionEffect(PotionEffectType.WITHER, config_.getInt("thorns_effect_duration"), config_.getInt("thorns_effect_intensity"));
+        	 potionEffect.apply(defender);
          }
         
          double newhealth = Math.max(0, (defender.getHealth() - damage_adjustment));
          defender.setHealth(newhealth);
-         
-         
+
          }
 
          @EventHandler(priority = EventPriority.LOWEST)
          public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-        	 DamageCause cause = event.getCause();
-        	 if (cause.equals(DamageCause.PROJECTILE)){
-        		 if (event.getDamager() instanceof Arrow){
-        			 Arrow arrow = (Arrow) event.getDamager();
-        			 if (arrow.getShooter() == null){
-        				 /*Arrow was fired from a dispenser
-        				  * damage of 18 is equivalent
-        				  * to a power V bow.
-        				  * 
-        				  * arrow.setFireTicks(1) makes it
-        				  * equivalent to a flame I bow
-        				  * */
-        				 event.setDamage(18);
-        			 }
+			 if (config_.getBoolean("buff_dispenser_damage", true))
+			 {
+				 DamageCause cause = event.getCause();
+				 if (cause.equals(DamageCause.PROJECTILE)){
+					 if (event.getDamager() instanceof Arrow){
+						 Arrow arrow = (Arrow) event.getDamager();
+						 if (arrow.getShooter() == null){
+
+							 /*Arrow was fired from a dispenser
+							  * damage of 18 is equivalent
+							  * to a power V bow.
+							  * */
+							 event.setDamage(config_.getInt("dispenser_damage"));
+        				 	}
+        			 	}
         			 }
         		 }
         	 }
@@ -156,7 +157,7 @@ public final class Protadjust extends JavaPlugin implements Listener {
         	 if (projectile instanceof Arrow){
         		 if (projectile.getShooter() == null){
         			 /*Arrow was fired by a dispenser*/
-        			 projectile.setFireTicks(500);
+        			 projectile.setFireTicks(config_.getInt("dispenser_flame_arrows_ticks"));
         			 
         			 Vector vector = projectile.getVelocity();
         			 double x = vector.getX();
@@ -165,26 +166,25 @@ public final class Protadjust extends JavaPlugin implements Listener {
         			 
         			 /*Makes dispensers less accurate*/
         			 if (y > 0.2){
-        				 y *= 3.5;
-        				 x *= 1.5;
-        				 z *= 1.5;
+        				 y *= config_.getDouble("dispenser_arrow_velocity_multiplier");
+        				 x *= config_.getDouble("dispenser_arrow_spread_multiplier") * 0.5;
+        				 z *= config_.getDouble("dispenser_arrow_spread_multiplier") * 0.5;
         				 }
         			 else if (x > z){
-        				 x *= 3;
-        				 getLogger().info("" + y);
+        				 x *= config_.getDouble("dispenser_arrow_spread_multiplier");
         				 }
         			 else{
-        				 z *= 3;
-        				 getLogger().info("" + y);
+        				 z *= config_.getDouble("dispenser_arrow_spread_multiplier");
         				 }
         			 
-        			 /*Makes the arrow travel faster*/
+        			 /*Makes the arrow travel faster and further*/
 
                      projectile.setVelocity(new Vector
-                             (x * 3.5, y, z * 3.5));
+                             (x * config_.getDouble("dispenser_arrow_velocity_multiplier")
+                            		 , y,
+                            		 z * config_.getDouble("dispenser_arrow_velocity_multiplier")));
+                     }
         		 }
-        	 }
-        		 
         	 }
          
          

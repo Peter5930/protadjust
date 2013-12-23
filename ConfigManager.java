28b17b900c7bde18457444d64d28da71918563b6
Peter5930 <peter.mackenzie@gmail.com>
2013-12-23 15:07:17 +0000

    Update ConfigManager.java

--- ConfigManager.java
+++ ConfigManager.java
@@ -85,6 +85,90 @@ public class ConfigManager {
         	config.set("dispenser_arrow_spread_multiplier", 3.0);
         }
         
+        /*Global damage modifiers.  These will increase or decrease the damage
+         * that gets applied to all armour types and to unarmoured players.*/
+        
+        if (!config.contains("BLOCK_EXPLOSION_global_modifier")){
+        	config.set("BLOCK_EXPLOSION_global_modifier", 2.0);
+        }
+        
+        if (!config.contains("CONTACT_global_modifier")){
+        	config.set("CONTACT_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("DROWNING_global_modifier")){
+        	config.set("DROWNING_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("ENTITY_ATTACK_global_modifier")){
+        	config.set("ENTITY_ATTACK_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("ENTITY_EXPLOSION_global_modifier")){
+        	config.set("ENTITY_EXPLOSION_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("FALL_global_modifier")){
+        	config.set("FALL_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("FALLING_BLOCK_global_modifier")){
+        	config.set("FALLING_BLOCK_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("FIRE_global_modifier")){
+        	config.set("FIRE_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("FIRE_TICK_global_modifier")){
+        	config.set("FIRE_TICK_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("LAVA_global_modifier")){
+        	config.set("LAVA_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("LIGHTNING_global_modifier")){
+        	config.set("LIGHTNING_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("MAGIC_global_modifier")){
+        	config.set("MAGIC_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("MELTING_global_modifier")){
+        	config.set("MELTING_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("POISON_global_modifier")){
+        	config.set("POISON_global_modifier", 0.45);
+        }
+        
+        if (!config.contains("PROJECTILE_global_modifier")){
+        	config.set("PROJECTILE_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("STARVATION_global_modifier")){
+        	config.set("STARVATION_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("SUFFOCATION_global_modifier")){
+        	config.set("SUFFOCATION_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("SUICIDE_global_modifier")){
+        	config.set("SUICIDE_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("THORNS_global_modifier")){
+        	config.set("THORNS_global_modifier", 1.0);
+        }
+        
+        if (!config.contains("VOID_global_modifier")){
+        	config.set("VOID_global_modifier", 1.0);
+        }
+        
+        
         /*Damage type modifiers specific to prot armour; values above 1.0 will
          * make prot weaker to that damage type and values below 1.0 will
          * make prot stronger to that damage type.*/
@@ -95,10 +179,6 @@ public class ConfigManager {
         
         if (!config.contains("CONTACT_prot_modifier")){
         	config.set("CONTACT_prot_modifier", 1.0);
-        }
-        
-        if (!config.contains("BLOCK_EXPLOSION_prot_modifier")){
-        	config.set("BLOCK_EXPLOSION_prot_modifier", 1.0);
         }
         
         if (!config.contains("DROWNING_prot_modifier")){
